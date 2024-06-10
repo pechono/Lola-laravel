95e13d3d7c4e66f967dad030387e180d94d8f7ca
Pechono <balsamo2@hotmail.com>
2024-06-10 12:34:10 -0300

    submenu stock

--- resources/views/stock/pedido.blade.php
+++ resources/views/stock/pedido.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout>
-    {{--  <x-slot name="header">
-         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-             {{ __('Cliente') }}
-         </h2>
-     </x-slot> --}}
+    <div class="mt-8">
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
+            {{-- --------MENU--------- --}}
+            @include('components.menu-stock')
+            {{-- ----------------- --}}
+        </div>
+    </div>
 
-     <div class="py-12">
+     <div class="mt-2">
          <div class="w-full mx-auto sm:px-6 lg:px-8">
              <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg ">
                <livewire:pedidolivewire/>
